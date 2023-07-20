4cb5fc44f7e3482f7b9fc4090d64358d80a3a37f
Darshan Dangar <darshanahir57267@gmail.com>
2023-07-20 12:02:38 +0530

    UserList Demo Completed

--- Chapter4/UserListDemo/app/src/main/java/com/example/userlistdemo/utils/ImageAdapter.kt
+++ Chapter4/UserListDemo/app/src/main/java/com/example/userlistdemo/utils/ImageAdapter.kt
@@ -5,7 +5,6 @@ import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
-import com.example.userlistdemo.R
 
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
@@ -14,7 +13,6 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
         Glide.with(imgView)
             .load(imgUri)
             .transform(CircleCrop())
-            .error(R.drawable.user)
             .into(imgView)
 
     }
