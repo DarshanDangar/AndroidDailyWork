cb3f9be859f90a430488c92711b87e77d9633392
Darshan Dangar <darshanahir57267@gmail.com>
2023-07-25 18:52:46 +0530

    add Profile Image in Profile fragment

--- Chapter3/DoctorChanneling/app/src/main/java/com/example/doctorchanneling/profile/ProfileFragment.kt
+++ Chapter3/DoctorChanneling/app/src/main/java/com/example/doctorchanneling/profile/ProfileFragment.kt
@@ -1,20 +1,27 @@
 package com.example.doctorchanneling.profile
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.doctorchanneling.R
-import com.example.doctorchanneling.helper.SharedPreferenceHelper
 import com.example.doctorchanneling.authentication.SignInActivity
 import com.example.doctorchanneling.databinding.FragmentProfileBinding
+import com.example.doctorchanneling.helper.SharedPreferenceHelper
+import java.io.File
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
+    var imageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,7 +43,25 @@ class ProfileFragment : Fragment() {
         binding.setting.image = images[4]
         binding.help.image = images[5]
         binding.logOut.image = images[6]
+        pickImage()
+        binding.imgProfile.setOnClickListener {
+            takePicture.launch(imageUri)
+        }
     }
+
+    private val takePicture =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { isPictureTaken ->
+            if (isPictureTaken) {
+                // Get the bitmap from the camera result and set it to the imgProfile ImageView
+                val bitmap =
+                    MediaStore.Images.Media.getBitmap(requireContext().contentResolver, imageUri)
+                Glide.with(requireContext())
+                    .load(bitmap)
+                    .centerCrop()
+                    .circleCrop()
+                    .into(binding.imgProfile)
+            }
+        }
 
     private fun logout() {
         binding.logOut.root.setOnClickListener {
@@ -47,4 +72,13 @@ class ProfileFragment : Fragment() {
         }
     }
 
+    private fun pickImage() {
+        val image = File(requireContext().filesDir, "camera_photo.png")
+        imageUri = FileProvider.getUriForFile(
+            requireContext(),
+            "com.example.doctorchanneling.fileProvider",
+            image
+        )
+
+    }
 }
